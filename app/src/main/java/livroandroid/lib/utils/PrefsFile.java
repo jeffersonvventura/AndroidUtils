995d04b763fddf87fe46eb37829613956ffc611f
Ricardo <rlecheta@gmail.com>
2015-02-02 11:04:22 -0200

    ok

--- app/src/main/java/livroandroid/lib/utils/PrefsFile.java
+++ app/src/main/java/livroandroid/lib/utils/PrefsFile.java
@@ -24,36 +24,36 @@ public class PrefsFile {
     public static final String PREF_ID = "prefs";
     private static final String TAG = "PrefsFile";
 
-    public static void setBoolean(Context context, String flag, boolean on) {
-        setString(context, flag, on ? "1" : "0");
+    public static void setBoolean(Context context, String chave, boolean on) {
+        setString(context, chave, on ? "1" : "0");
     }
 
-    public static boolean getBoolean(Context context, String flag) {
-        String s = getString(context, flag);
+    public static boolean getBoolean(Context context, String chave) {
+        String s = getString(context, chave);
         boolean on = "1".equals(s);
         return on;
     }
 
-    public static void setInteger(Context context, String flag, int valor) {
-        setString(context, flag, String.valueOf(valor));
+    public static void setInteger(Context context, String chave, int valor) {
+        setString(context, chave, String.valueOf(valor));
     }
 
-    public static int getInteger(Context context, String flag) {
-        String s = getString(context, flag);
+    public static int getInteger(Context context, String chave) {
+        String s = getString(context, chave);
         if (s != null) {
             return Integer.parseInt(s);
         }
         return 0;
     }
 
-    public static void setString(Context context, String flag, String valor) {
+    public static void setString(Context context, String chave, String valor) {
         try {
-            File f = context.getFileStreamPath(flag);
+            File f = context.getFileStreamPath(chave);
             if (f.exists()) {
                 Log.d(TAG, "PrefsFile.setString delete file: " + f);
                 f.delete();
             }
-            FileOutputStream out = context.openFileOutput(flag, Context.MODE_PRIVATE);
+            FileOutputStream out = context.openFileOutput(chave, Context.MODE_PRIVATE);
             DataOutputStream dataOut = new DataOutputStream(out);
             dataOut.writeUTF(valor);
             dataOut.close();
@@ -63,12 +63,12 @@ public class PrefsFile {
         }
     }
 
-    public static String getString(Context context, String flag) {
+    public static String getString(Context context, String chave) {
         try {
-            File f = context.getFileStreamPath(flag);
+            File f = context.getFileStreamPath(chave);
             Log.d(TAG, "PrefsFile.getString file: " + f);
             if (f.exists()) {
-                FileInputStream in = context.openFileInput(flag);
+                FileInputStream in = context.openFileInput(chave);
                 DataInputStream dataIn = new DataInputStream(in);
                 String s = dataIn.readUTF();
                 Log.d(TAG, "PrefsFile.getString s: " + s);
--- app/src/main/java/livroandroid/lib/utils/PrefsSDCard.java
+++ app/src/main/java/livroandroid/lib/utils/PrefsSDCard.java
@@ -11,30 +11,30 @@ import java.io.File;
 public class PrefsSDCard {
     public static final String PREF_ID = "prefs";
     private static final String TAG = "PrefsSDCard";
-    public static void setBoolean(Context context, String flag, boolean on) {
-        setString(context, flag, on ? "1" : "0");
+    public static void setBoolean(Context context, String chave, boolean on) {
+        setString(context, chave, on ? "1" : "0");
     }
-    public static boolean getBoolean(Context context, String flag) {
-        String s = getString(context, flag);
+    public static boolean getBoolean(Context context, String chave) {
+        String s = getString(context, chave);
         boolean on = "1".equals(s);
         return on;
     }
-    public static void setInteger(Context context, String flag, int valor) {
-        setString(context,flag,String.valueOf(valor));
+    public static void setInteger(Context context, String chave, int valor) {
+        setString(context,chave,String.valueOf(valor));
     }
-    public static int getInteger(Context context, String flag) {
-        String s = getString(context,flag);
+    public static int getInteger(Context context, String chave) {
+        String s = getString(context,chave);
         if(s != null) {
             return Integer.parseInt(s);
         }
         return 0;
     }
-    public static void setString(Context context, String flag, String valor) {
-        File f = SDCardUtils.getPublicFile("prefs", flag + ".txt");
+    public static void setString(Context context, String chave, String valor) {
+        File f = SDCardUtils.getPublicFile("prefs", chave + ".txt");
         IOUtils.writeString(f, valor);
     }
-    public static String getString(Context context, String flag)  {
-        File f = SDCardUtils.getPublicFile("prefs",flag+".txt");
+    public static String getString(Context context, String chave)  {
+        File f = SDCardUtils.getPublicFile("prefs",chave+".txt");
         Log.d("livroandroid", "getString: " + f);
         return IOUtils.readString(f);
     }
