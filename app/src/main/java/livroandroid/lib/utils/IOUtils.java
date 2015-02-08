92df5489aa197791e9f6f81e3d53537e65603ecb
Ricardo <rlecheta@gmail.com>
2015-02-08 11:51:40 -0200

    ok

--- app/src/main/java/livroandroid/lib/utils/IOUtils.java
+++ app/src/main/java/livroandroid/lib/utils/IOUtils.java
@@ -1,6 +1,9 @@
 package livroandroid.lib.utils;
 
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Looper;
 import android.util.Log;
 
 import java.io.ByteArrayOutputStream;
@@ -14,6 +17,13 @@ import java.io.InputStream;
 public class IOUtils {
     private static final String TAG = "IOUtils";
 
+    /**
+     * Converte a InputStream para String utilizando o charset informado
+     * @param in
+     * @param charset UTF-8 ou ISO-8859-1
+     * @return
+     * @throws IOException
+     */
     public static String toString(InputStream in, String charset)
             throws IOException {
         byte[] bytes = toBytes(in);
@@ -21,6 +31,11 @@ public class IOUtils {
         return texto;
     }
 
+    /**
+     * Converte a InputStream para bytes[]
+     * @param in
+     * @return
+     */
     public static byte[] toBytes(InputStream in) {
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         try {
@@ -44,24 +59,16 @@ public class IOUtils {
         }
     }
 
+    /**
+     * Salva o texto em arquivo
+     * @param file
+     * @param valor
+     */
     public static void writeString(File file, String valor) {
         try {
             FileOutputStream fout = new FileOutputStream(file);
             fout.write(valor.getBytes());
             fout.close();
-        } catch (IOException e) {
-            Log.e(TAG, e.getMessage(), e);
-        }
-    }
-
-    public static void writeBitmap(File file, Bitmap bitmap) {
-        try {
-            if(!file.exists()) {
-                file.createNewFile();
-            }
-            FileOutputStream out = new FileOutputStream(file);
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-            out.close();
         } catch (IOException e) {
             Log.e(TAG, e.getMessage(), e);
         }
@@ -77,6 +84,71 @@ public class IOUtils {
         } catch (IOException e) {
             Log.e(TAG, e.getMessage(), e);
             return null;
+        }
+    }
+
+    /**
+     * Salva a figura em aruqivo
+     * @param file
+     * @param bitmap
+     */
+    public static void writeBitmap(File file, Bitmap bitmap) {
+        try {
+            if(!file.exists()) {
+                file.createNewFile();
+            }
+            FileOutputStream out = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.close();
+        } catch (IOException e) {
+            Log.e(TAG, e.getMessage(), e);
+        }
+    }
+
+    public interface Callback {
+        public void onFileSaved(File file, boolean exists);
+    }
+
+    /**
+     * Salva o bitmap em arquivo. Utiliza a URL para descobrir o nome.
+     *
+     * @param dir Diretório para criar no sd card
+     * @param url URL original para extrair o nome do arquivo
+     * @param bitmap Bitmap que já está em memória
+     * @param callback Interface de retorno
+     */
+    public static void saveBitmapToFile(String dir, String url,Bitmap bitmap,Callback callback) {
+        if(Looper.myLooper() == Looper.getMainLooper()) {
+            throw new RuntimeException("O método saveBitmapToFile não pode ser executado na UI Thread.");
+        }
+        try {
+            if(url == null || bitmap == null && callback != null) {
+                return;
+            }
+
+            String fileName = url.substring(url.lastIndexOf("/"));
+
+            File file = SDCardUtils.getPublicFileWithType(dir, fileName, Environment.DIRECTORY_PICTURES);
+            if(file.exists()) {
+                callback.onFileSaved(file,true);
+            }
+            else {
+                FileOutputStream out = new FileOutputStream(file);
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+                out.close();
+
+                Log.d(TAG, "Save File: " + file);
+
+                // Salva o arquivo
+                IOUtils.writeBitmap(file, bitmap);
+
+                callback.onFileSaved(file,false);
+            }
+
+
+
+        } catch (IOException e) {
+            Log.e(TAG, e.getMessage(), e);
         }
     }
 }
