586d6f658537bd6095124ca523f8860ecf2e16b8
livroandroidup@gmail.com <livroandroidup@gmail.com>
2015-02-11 01:27:57 -0200

    ok

--- app/src/main/java/livroandroid/lib/utils/FIleUtils.java
+++ app/src/main/java/livroandroid/lib/utils/FIleUtils.java
@@ -4,8 +4,13 @@ import android.content.Context;
 import android.content.res.AssetManager;
 import android.content.res.Resources;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * Created by ricardo on 11/02/15.
@@ -50,5 +55,51 @@ public class FIleUtils {
         InputStream in = assets.open(fileName);
         String s = IOUtils.toString(in, charset);
         return s;
+    }
+
+    /**
+     * Retorna o caminho do arquivo para ser salvo na memória interna.
+     *
+     * Obs: Apenas retorna o caminho do arquivo, mas não o cria.
+     *
+     * Ex: /data/data/br.com.livroandroid.carros/files/arquivoi.txt
+     *
+     * @param context
+     * @param name
+     * @return
+     */
+    public static File getPrivateFile(Context context,String name) {
+        File file = context.getFileStreamPath(name);
+        return file;
+    }
+
+    /**
+     * Retorna o Outputstream para salvar o arquivo na memória interna.
+     *
+     * Ex: /data/data/br.com.livroandroid.carros/files/arquivoi.txt
+     *
+     * @param context
+     * @param name
+     * @return
+     */
+    public static OutputStream getPrivateFileOutput(Context context,String name) throws FileNotFoundException {
+        // Cria o arquivo e retorna o OutputStream
+        FileOutputStream out = context.openFileOutput(name, Context.MODE_PRIVATE);
+        return out;
+    }
+
+    /**
+     * Retorna a InputStream para ler o arquivo da memória interna.
+     *
+     * Ex: /data/data/br.com.livroandroid.carros/files/arquivoi.txt
+     *
+     * @param context
+     * @param name
+     * @return
+     * @throws FileNotFoundException
+     */
+    public static InputStream getPrivateFileInputStream(Context context,String name) throws FileNotFoundException {
+        FileInputStream in = context.openFileInput(name);
+        return  in;
     }
 }
