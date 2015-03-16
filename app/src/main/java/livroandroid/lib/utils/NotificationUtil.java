9c60875c4de7f69cbfc51c1ba738316e8c1ccf5e
Ricardo <rlecheta@gmail.com>
2015-03-16 20:52:23 -0300

    ok

---
+++ app/src/main/java/livroandroid/lib/utils/NotificationUtil.java
@@ -0,0 +1,41 @@
+package livroandroid.lib.utils;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import br.com.livroandroid.androidutils.R;
+
+/**
+ * Classe utilitária para disparar notifications
+ */
+public class NotificationUtil {
+
+    private static final String TAG = "livroandroid";
+
+    public static void create(Context context, int id, Intent intent,int smallIcon, String contentTitle, String contentText) {
+        NotificationManager manager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Intent para disparar o broadcast
+        PendingIntent p = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Cria a notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setContentIntent(p)
+                .setContentTitle(contentTitle)
+                .setContentText(contentText)
+                .setSmallIcon(smallIcon)
+                .setAutoCancel(true);
+
+        // Dispara a notification
+        Notification n = builder.build();
+        manager.notify(id, n);
+
+        Log.d(TAG,"Notification criada com sucesso");
+    }
+}
